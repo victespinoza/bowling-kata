7e09a3287692a6c8aaa7b400f2e436cce7001e97
victespinoza <victespinoza90@gmail.com>
2023-07-16 21:16:31 -0300

    Added max quantity of rolls

--- src/main/Game.ts
+++ src/main/Game.ts
@@ -1,10 +1,18 @@
 export default class Game {
-    roll(pines: number) {
-        
+    private globalScore = 0;
+    private turns = 1;
+
+    constructor(private max_turns: number){}
+
+    public roll(droppedPines: number) {
+        if (this.turns > this.max_turns)
+            return;
+        this.globalScore += droppedPines;
+        this.turns++;
     }
     
 
     public score(): number {
-        return 8;
+        return this.globalScore;
     }
 }
--- src/test/Game.test.ts
+++ src/test/Game.test.ts
@@ -1,10 +1,26 @@
 import Game from "../main/Game"
 
 describe('Game',()=>{
+    const MAX_TURNS = 20;
+
     it ('Should return the right score when roll are played',()=>{
-        const game = new Game();
+        const game = new Game(MAX_TURNS);
+
         game.roll(8);
+
         expect(game.score()).toBe(8);
     })
 
+    
+    it ('Should allow a max of 20 rolls',()=>{
+        const game = new Game(MAX_TURNS);
+        const turns = 25;
+        const droppedPines = 4;
+
+        for(let i=0; i<turns; i++){
+            game.roll(droppedPines);
+        }
+
+        expect(game.score()).toBe(MAX_TURNS*droppedPines);
+    })
 })
